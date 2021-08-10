d7d04f967333fa8037dafbad1bb2346c5891508a
zhantech <hifzhan41@gmail.com>
2021-08-10 09:28:00 +0700

    mojito: Remove Some Packages

--- Bloatware/Android.mk
+++ Bloatware/Android.mk
@@ -26,7 +26,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Chrome Chrome-Stub Drive Maps PrebuiltGmail Videos YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt DevicePolicyPrebuilt Drive GoogleCamera MyVerizonServices Ornament OBDM_Permissions OemDmTrigger PixelWallpapers2020 PixelLiveWallpaperPrebuilt SafetyHubPrebuilt ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt VZWAPNLib VzwOmaTrigger WallpapersBReel2020 obdm_stub  Chrome Chrome-Stub Drive Maps PrebuiltGmail Videos YouTube YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
