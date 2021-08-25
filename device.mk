a60eabfbe660bd3099bf1ac33438d4bd322c60ba
zhantech <hifzhan41@gmail.com>
2021-08-25 01:40:10 +0000

    mojito: Add Bloatware and Remove Some Packages

--- device.mk
+++ device.mk
@@ -52,6 +52,12 @@ PRODUCT_COPY_FILES += \
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
+# Bloatware
+PRODUCT_PACKAGES += \
+    GCam \
+    GalleryGo \
+    RemovePackages
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
