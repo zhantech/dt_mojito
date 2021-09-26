bf5d5e12ff9cd9deafebb67a89c5c3ef8e2655a9
zhantech <hifzhan41@gmail.com>
2021-09-26 12:38:40 +0000

    mojito: Kang configs patch from LOS * Kang from los Signed-off-by: anggialansori404 <anggialansori.rpl4@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -108,7 +108,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_mojito
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    $(DEVICE_PATH)/configs/hidl/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += \
     $(DEVICE_PATH)/configs/hidl/manifest.xml \
     hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
